40e3f5296f39825d0d4394aa113f480bb21e4f83
Andrew Fernandez <fdez26@ymail.com>
2019-04-13 19:55:48 -0400

    start main file implementation

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Customer.hpp"
+#include "Account.hpp"
+#include "Transaction.hpp"
+#include "ATM.hpp"
+
+#include <iostream>
+
+void printWelcomeScreen() {
+    for (int i = 0; i < 10; i++) {
+        std::cout << '*';
+    }
+    std::endl;
+}
+int main() {
+
+    return 0;
+}
