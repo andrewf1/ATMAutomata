4c681d59c57e37fa387c553c04b4829ebd50acf1
Andrew Fernandez <fdez26@ymail.com>
2019-04-13 16:46:27 -0400

    work on Account and Customer classes, also created a temporary createCustomer function to generate fake Customers to be called in main

--- Account.hpp
+++ Account.hpp
@@ -6,8 +6,8 @@ class Account
     private:
         long accountNum;
         double balance;
-
     public:
+        Account(long acc_num, double bal) { accountNum = acc_num; balance = bal; }
         double getBalance() {return balance;}
         void deposit(double x) {balance += x;}
         void withdraw(int x) {balance -= x;}
