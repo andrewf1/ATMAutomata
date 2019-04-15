01ae19e2ee1e18d20165c8551e24992aab6c2ffd
Anthony Reveron <anthrev96@gmail.com>
2019-04-14 20:58:10 -0400

    Worked more on Transaction class

--- Transaction.cpp
+++ Transaction.cpp
@@ -1,13 +1,24 @@
 #include "Transaction.hpp"
 
 #include <iostream>
+#include <iomanip>
 
 void Transaction::print()
 {
     std::cout << "Customer: " << customer.getName() << "    Account Number: " << customer.accountNum() << std::endl;
     std::cout << "Start Balace: " << startBalance << std::endl;
-    for(auto i = exchanges.begin(); i != exchanges.end(); i++)
+    std::cout << std::setprecision(2);
+    for(auto it = exchanges.begin(); it != exchanges.end(); it++)
     {
-
+        std::cout << '\t' << it->first << ' ' << it->second << std::endl;
     }
+    std::cout << "End Balance: " << currentBalance << std::endl;
+}
+
+void Transaction::editTran(char operand, double amount)
+{
+    if(operand == '-') currentBalance -= amount;
+    else currentBalance += amount;
+    std::pair<char, double> temp(operand, amount);
+    exchanges.push_back(temp);
 }
